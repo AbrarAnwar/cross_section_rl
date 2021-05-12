7379b873e66ae2cd5131640d0637f9ab18e2f9f7
phuong minh nguyen <minhpnguyen@utexas.edu>
2021-05-12 00:29:19 -0500

    Update HOT functional

--- utils2.py
+++ utils2.py
@@ -3,18 +3,32 @@ from numpy.linalg import norm, det
 from math import acos, tan
 import itertools
 
-from sympy.geometry import Point3D, Line3D
-from sympy import intersection
 import pyvista as pv
 from pyvista import examples
+import trimesh
 
-from utils import delaunay_triangulation
+import utils
 
+######## ROUTINES ##########
 def cot1(x, y):
     return 1.0/tan(acos(np.dot(x, y)/(norm(x)*norm(y))))
 
 def area_vec(x, y):
     return norm(np.cross(x, y))/2
+
+def check_collinear(x, y):
+    return abs(abs(np.dot(x, y)) - norm(x)*norm(y)) < 1e-9
+
+def intersection(p1, d1, p2, d2):
+    p3 = p2 - p1
+    ratio = norm(np.cross(p3, d2))/norm(np.cross(d1, d2))
+    c1 = p1 + ratio*d1
+    c2 = p1 - ratio*d1
+    #print(check_collinear(c1-p2, d2) or check_collinear(c2-p2, d2))
+    if check_collinear(c1-p2, d2): 
+        return c1
+    
+    return c2
 
 ###################### COMPUTE HOT_2, 2 functionals of a triangle #############################
 # v: list of vertices. v[i] is 3D-vector coordinate of ith vertex
@@ -72,16 +86,13 @@ def circumcenter_tri(v, w):
     # First compute circumcenters of segment or 1-simplex. Hence the name c1
     c1 = circumcenter_line(v, w)
     # Compute circumcenter of triangle or 2-simplex by orthogonal lines
-    line = []
+    d = np.zeros((2, 3)) # two orthgonal directions
     ind1 = np.array([0, 1, 2], dtype=int)
     for k in range(2):
         ind = np.delete(ind1, k)
-        d = np.cross(v[ind[0]]-v[ind[1]], normal_v)
-        line.append(Line3D(Point3D(c1[k], evaluate=False), direction_ratio=d))
-    #print(line[0], line[1])
-    c2 = intersection(line[0], line[1])[0]
-    
-    return np.array([c2.x, c2.y, c2.z], dtype=float)
+        d[k] = np.cross(v[ind[0]]-v[ind[1]], normal_v)
+
+    return intersection(c1[0], d[0], c1[1], d[1])
        
 def circumcenter_tetra(v, w):
     # circumcenters of triangle or 2-simplex. Hence the name c2
@@ -92,14 +103,14 @@ def circumcenter_tetra(v, w):
         # If the tetrahedron is ijlk then this c2[k] is weighted circumcenter of ijl
         c2[k] = circumcenter_tri(v1, w1)
     # Again compute circumcenter of tetrahedron or 3-simplex by orthogonal lines
-    line = []; ind1 = np.array([0, 1, 2, 3], dtype=int)
+    normal_ijl = np.zeros((2, 3)) # orthogonal directions
+    ind1 = np.array([0, 1, 2, 3], dtype=int)
     for l in range(2):
         ind = np.delete(ind1, l)
         i, j, k = ind[0], ind[1], ind[2]
-        normal_ijl = np.cross(v[i]-v[j], v[i]-v[k])
-        line.append(Line3D(Point3D(c2[k], evaluate=False), direction_ratio=normal_ijl))
-    c3 = intersection(line[0], line[1])[0]
-    return c2, np.array([c3.x, c3.y, c3.z], dtype=float)
+        normal_ijl[l] = np.cross(v[i]-v[j], v[i]-v[k])
+
+    return c2, intersection(c2[0], normal_ijl[0], c2[1], normal_ijl[1])
 
 def signed_dist_c23(v, w):
     H = np.zeros(4)
@@ -162,35 +173,51 @@ def compute_HOT_tetra(v, w, degree=[1, 0, 0, 0]):
         ret += (H[l]**3)*h[l, k]*d[i, j]*coef[0] + H[l]*(h[l, k]**3)*d[i, j]*coef[1] \
             + H[l]*h[l, k]*(d[i, j]**3)*coef[2]
     return ret
-    
-# HOT functional for (Delaunay triangulation) from set of points. Currently assume all weights are zero
+
+##################################################################################################
+########## HOT functional for (Delaunay triangulation) from set of points and weights ############
+##################################################################################################
+# vertices: set of vertex points
+# weights: set of corresponding weights
 # degree2 = weight combination for HOT functional on 2-simplices (triangle)
 # degree3 = weight combination for HOT functional on 3-simplices (tetrahedron)
 
-def compute_HOT(pts, degree2=[0, 1, 2], degree3=[1, 0, 0, 0]):
+def compute_HOT(vertices, weights, degree2=[0, 1, 2], degree3=[1, 0, 0, 0]):
+    tetra_faces = utils.WeightedDelaunay(vertices, weights)
+    tri_faces = utils.get_surface_tris_from_tet(tetra_faces)
     HOT = 0
-    tri_faces, tetra_faces = delaunay_triangulation(pts)
+    # Go over each triangle and tetrahedron and sum up all HOT functionals (with correponding degrees)
     for tri in tri_faces:
-        v = np.array([pts[tri[0]], pts[tri[1]], pts[tri[2]]])
-        w = np.zeros(3)
+        v = np.array([vertices[tri[0]], vertices[tri[1]], vertices[tri[2]]])
+        w = np.array([weights[tri[0]], weights[tri[1]], weights[tri[2]]])
         HOT += compute_HOT_tri(v, w, degree2)
     for tet in tetra_faces:
-        v = np.array([pts[tet[0]], pts[tet[1]], pts[tet[2]], pts[tet[3]]])
-        w = np.zeros(4)
+        v = np.array([vertices[tet[0]], vertices[tet[1]], vertices[tet[2]], vertices[tet[3]]])
+        w = np.array([weights[tet[0]], weights[tet[1]], weights[tet[2]], weights[tet[3]]])
         HOT += compute_HOT_tetra(v, w, degree3)
         
     return HOT
     
-# Load mesh
-
+# EXAMPLES of using compute_HOT
 vertices = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1]], dtype=float)
-HOT = compute_HOT(vertices)
+num_points = len(vertices)
+w = 10*np.random.rand(num_points)
+degree3=[1, 0, 0, 0]
+HOT = compute_HOT(vertices, w, degree3=degree3)
 print(f'The (combined) hot functional is {HOT}')
+
 '''
 UNIT_TESTS:
+# Test circumcenter_tetra
 v = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1]], dtype=float)
 w = np.zeros(4)
 print(circumcenter_tetra(v, w))
+# Test intersection
+p1 = np.array([0, 1, 2], dtype=float)
+p2 = np.array([0, 1, 4], dtype=float)
+d1 = np.array([0, 3, 2], dtype=float)
+d2 = np.array([0, 22, 2], dtype=float)
+print(intersection(p1, d1, p2, d2))
 '''
 
 ''' Unweighted circumcenters
